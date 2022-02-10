bd5387338e137248b0b4d19ee8e4f634ebe2f941
sharshenovra <67783261+sharshenovra@users.noreply.github.com>
2022-02-10 18:44:31 +0600

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Ruslan Sharshenov on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
